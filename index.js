74b0e48d6d36b9c46e124f9ebdaeca0ee1a59fe7
Mayank-3e <7mayank.agarwal@gmail.com>
2023-02-12 18:27:51 +0530

    server config

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.json())
 app.use((req,res,next)=>
 {
     res.setHeader('Access-Control-Allow-Origin','*')
-    res.setHeader('Access-Control-Allow-Methods','GET,POST')
-    res.setHeader('Access-Control-Allow-Headers','Content-Type')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
     next()
 })
 
